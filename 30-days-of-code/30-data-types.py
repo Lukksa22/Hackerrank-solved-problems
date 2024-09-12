635482682df65c3b53f4a5adeb5eae139d73ac8c
Lukksa22 <la1247890@gmail.com>
2024-09-12 14:32:13 -0300

    added 30-days-of-code

---
+++ 30-days-of-code/30-data-types.py
@@ -0,0 +1,17 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+
+# Read and save an integer, double, and String to your variables.
+int_var = int(input())
+dbl_var = float(input())
+str_var = input()
+# Print the sum of both integer variables on a new line.
+print(int_var+i)
+# Print the sum of the double variables on a new line.
+print(dbl_var+d)
+
+# Concatenate and print the String variables on a new line
+print(s+str_var)
+# The 's' variable above should be printed first.
